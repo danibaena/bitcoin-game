a92faa4ce19363ae90ae5af2bc8d776855c601d4
Daniel Baena <d.baena.hermosa@gmail.com>
2025-03-22 20:36:43 +0100

    chore: Add delay for dev mode mocked requests

--- apps/frontend/src/mocks/handlers.ts
+++ apps/frontend/src/mocks/handlers.ts
@@ -1,8 +1,9 @@
 import { API_URL } from "@/hooks"
-import { http, HttpResponse } from "msw"
+import { delay, http, HttpResponse } from "msw"
 
 export const handlers = [
   http.get(API_URL, async () => {
+    await delay(800)
     // Generate random number for bitcoin price between 90000 and 80000 with 2 decimals in dev mode
     return HttpResponse.json({ price: (Math.random() * (90000 - 80000) + 80000).toFixed(2), source: "api" }, { status: 200 })
   }),
