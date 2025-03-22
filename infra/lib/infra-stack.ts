fb4e6337b3c526cd48d575ea9579cf119c2f5e80
Daniel Baena <d.baena.hermosa@gmail.com>
2025-03-22 04:40:15 +0100

    build: Add infra for lambdas, api gw and dynamo

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,7 +1,9 @@
 import * as cdk from "aws-cdk-lib"
+import * as apigateway from "aws-cdk-lib/aws-apigateway"
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront"
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins"
-
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb"
+import * as lambda from "aws-cdk-lib/aws-lambda"
 import * as s3 from "aws-cdk-lib/aws-s3"
 import { Construct } from "constructs"
 
@@ -27,6 +29,72 @@ export class BitcoinGameStack extends cdk.Stack {
       },
       defaultRootObject: "index.html",
     })
+
+    const playersTable = new dynamodb.Table(this, "PlayersTable", {
+      partitionKey: { name: "sessionId", type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      timeToLiveAttribute: "lastActive",
+    })
+
+    const getBitcoinPriceLambda = new lambda.Function(this, "GetBitcoinPriceLambda", {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset("../../apps/backend/src/lambdas/getBitcoinPrice"),
+      environment: {},
+    })
+
+    const getOrCreateSessionLambda = new lambda.Function(this, "GetOrCreateSessionLambda", {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset("../../apps/backend/src/lambdas/getOrCreateSession"),
+      environment: {
+        TABLE_NAME: playersTable.tableName,
+      },
+    })
+
+    const updateScoreLambda = new lambda.Function(this, "UpdateScoreLambda", {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset("../../apps/backend/src/lambdas/updateScore"),
+      environment: {
+        TABLE_NAME: playersTable.tableName,
+      },
+    })
+
+    playersTable.grantReadWriteData(getOrCreateSessionLambda)
+    playersTable.grantReadWriteData(updateScoreLambda)
+
+    const api = new apigateway.RestApi(this, "BitcoinGameApi", {
+      restApiName: "BitcoinGameService",
+      deployOptions: {
+        stageName: "prod",
+      },
+    })
+
+    api.root.addResource("price").addMethod("GET", new apigateway.LambdaIntegration(getBitcoinPriceLambda), {
+      methodResponses: [{ statusCode: "200" }],
+    })
+
+    api.root.addResource("session").addMethod("GET", new apigateway.LambdaIntegration(getOrCreateSessionLambda), {
+      methodResponses: [{ statusCode: "200" }],
+    })
+
+    api.root.addResource("score").addMethod("POST", new apigateway.LambdaIntegration(updateScoreLambda), {
+      methodResponses: [{ statusCode: "200" }],
+    })
+
+    distribution.addBehavior(
+      "/api/*",
+      new origins.HttpOrigin(`${api.restApiId}.execute-api.${this.region}.amazonaws.com`, {
+        originPath: "/prod",
+      }),
+      {
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+        cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+    )
 
     new cdk.CfnOutput(this, "CloudFrontURL", {
       value: distribution.domainName,
