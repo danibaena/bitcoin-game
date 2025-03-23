723afb580445c0d1c96d8468e8c9a084fdbeae9c
Daniel Baena <d.baena.hermosa@gmail.com>
2025-03-23 23:49:43 +0100

    build: Add correct behaviors for origin request

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -96,8 +96,9 @@ export class BitcoinGameStack extends cdk.Stack {
         allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
         cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
         originRequestPolicy: new cloudfront.OriginRequestPolicy(this, "ApiOriginRequestPolicy", {
-          cookieBehavior: cloudfront.OriginRequestCookieBehavior.allowList("sessionId"),
-          headerBehavior: cloudfront.OriginRequestHeaderBehavior.all(),
+          cookieBehavior: cloudfront.OriginRequestCookieBehavior.all(),
+          headerBehavior: cloudfront.OriginRequestHeaderBehavior.none(),
+          queryStringBehavior: cloudfront.OriginRequestQueryStringBehavior.none(),
         }),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
         responseHeadersPolicy: cloudfront.ResponseHeadersPolicy.CORS_ALLOW_ALL_ORIGINS_WITH_PREFLIGHT_AND_SECURITY_HEADERS,
