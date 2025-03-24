fce760fbd41fe73e535616ba45374fc42d10d4f5
Daniel Baena <d.baena.hermosa@gmail.com>
2025-03-24 19:27:59 +0100

    refactor: Improve result display with more info

--- apps/frontend/src/components/GuessResultDisplay.tsx
+++ apps/frontend/src/components/GuessResultDisplay.tsx
@@ -10,7 +10,6 @@ export const GuessResultDisplay = () => {
     price: { priceAtGuessTime, comparedPrice },
   } = useGame()
   const [showResult, setShowResult] = useState(false)
-  const isPriceUnchanged = priceAtGuessTime === comparedPrice
 
   useEffect(() => {
     if (guessResolved) {
@@ -30,21 +29,43 @@ export const GuessResultDisplay = () => {
 
   return (
     <div className={cn("mt-4 flex flex-col gap-3 rounded-xl p-4 text-center", isLastGuessCorrect ? "bg-green-100" : "bg-red-100")}>
-      <p className="text-lg font-bold">
-        {isLastGuessCorrect ? "Correct guess!" : "Incorrect guess!"}
-        {isPriceUnchanged ? " Price remained the same 🟰" : lastGuessDirection === GuessDirection.up ? " Price went higher 📈" : " Price went lower 📉"}
-      </p>
-      <div>
-        <div className="flex items-center justify-between gap-8">
-          <p className="text-black">Price at guessing:</p>
-          <span className="text-orange">${priceAtGuessTime?.toLocaleString()}</span>
+      <p className="text-lg font-bold">{isLastGuessCorrect ? "Correct guess!" : "Incorrect guess!"}</p>
+      <div className="flex flex-col gap-2">
+        <div>
+          <div className="flex items-center justify-between gap-8">
+            <p className="text-black">Your guess:</p>
+            <span>{lastGuessDirection === GuessDirection.up ? " Price will go higher 📈" : " Price will go lower 📉"}</span>
+          </div>
+          <div className="flex items-center justify-between gap-8">
+            <p className="text-black">Result:</p>
+            <PriceMessage priceAtGuessTime={priceAtGuessTime} comparedPrice={comparedPrice} />
+          </div>
         </div>
-        <div className="flex items-center justify-between gap-8">
-          <p className="text-black">Price compared:</p>
-          <span className="text-orange">${comparedPrice?.toLocaleString()}</span>
+        <div>
+          <div className="flex items-center justify-between gap-8">
+            <p className="text-black">Price at guessing:</p>
+            <span className="text-orange">${priceAtGuessTime?.toLocaleString()}</span>
+          </div>
+          <div className="flex items-center justify-between gap-8">
+            <p className="text-black">Price compared:</p>
+            <span className="text-orange">${comparedPrice?.toLocaleString()}</span>
+          </div>
         </div>
       </div>
       <p className="text-bold text-2xl">{isLastGuessCorrect ? "+1 point" : "-1 point"}</p>
     </div>
   )
+}
+
+type PriceMessageProps = {
+  priceAtGuessTime: number | null
+  comparedPrice: number | null
+}
+
+const PriceMessage = ({ priceAtGuessTime, comparedPrice }: PriceMessageProps) => {
+  if (priceAtGuessTime === comparedPrice && priceAtGuessTime !== null) {
+    return <span>" Price remained the same 🟰"</span>
+  }
+
+  return <span>{priceAtGuessTime! < comparedPrice! ? " Price went higher 📈" : " Price went lower 📉"}</span>
 }
